ce3d878e8bcb6a6af842716743cb1422ff8288ca
Mangala Kodagoda <mail.mangala@gmail.com>
2019-10-31 13:04:28 +1100

    Generate random password.

--- IdentityServer4Org/Areas/Admin/Controllers/UserController.cs
+++ IdentityServer4Org/Areas/Admin/Controllers/UserController.cs
@@ -44,7 +44,7 @@ namespace IdentityServer4Org.Areas.Admin.Controllers
                         Email = model.Email
                     };
 
-                    string temporaryPassword = "Password123$"; //TODO: generate random password
+                    string temporaryPassword = GenerateRandomPassword(userManager.Options.Password);
                     var result = await userManager.CreateAsync(user, temporaryPassword);
 
                     if (result.Succeeded)
@@ -123,6 +123,47 @@ namespace IdentityServer4Org.Areas.Admin.Controllers
                 ModelState.AddModelError("", "Invalid Request");
             }
             return View();
+        }
+
+        private static string GenerateRandomPassword(PasswordOptions opts)
+        {
+            //https://www.ryadel.com/en/c-sharp-random-password-generator-asp-net-core-mvc/
+
+            string[] randomChars = new[] {
+                "ABCDEFGHJKLMNOPQRSTUVWXYZ",    // uppercase 
+                "abcdefghijkmnopqrstuvwxyz",    // lowercase
+                "0123456789",                   // digits
+                "!@$?_-"                        // non-alphanumeric
+            };
+
+            Random rand = new Random(Environment.TickCount);
+            List<char> chars = new List<char>();
+
+            if (opts.RequireUppercase)
+                chars.Insert(rand.Next(0, chars.Count),
+                    randomChars[0][rand.Next(0, randomChars[0].Length)]);
+
+            if (opts.RequireLowercase)
+                chars.Insert(rand.Next(0, chars.Count),
+                    randomChars[1][rand.Next(0, randomChars[1].Length)]);
+
+            if (opts.RequireDigit)
+                chars.Insert(rand.Next(0, chars.Count),
+                    randomChars[2][rand.Next(0, randomChars[2].Length)]);
+
+            if (opts.RequireNonAlphanumeric)
+                chars.Insert(rand.Next(0, chars.Count),
+                    randomChars[3][rand.Next(0, randomChars[3].Length)]);
+
+            for (int i = chars.Count; i < opts.RequiredLength
+                || chars.Distinct().Count() < opts.RequiredUniqueChars; i++)
+            {
+                string rcs = randomChars[rand.Next(0, randomChars.Length)];
+                chars.Insert(rand.Next(0, chars.Count),
+                    rcs[rand.Next(0, rcs.Length)]);
+            }
+
+            return new string(chars.ToArray());
         }
     }
 }
